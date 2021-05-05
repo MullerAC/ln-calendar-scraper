8f3fbc3dcf42e206de1fb557a5e19c2754693810
Andrew Muller <andrew.muller@utexas.edu>
2021-05-05 16:55:16 -0500

    fixed to work with new file structure, cleaned some scrapers

--- scrape/cross_infinite_world.py
+++ scrape/cross_infinite_world.py
@@ -1,11 +1,16 @@
-#from bs4 import BeautifulSoup
+"""
+TO DO:
+- 
+"""
+
+from bs4 import BeautifulSoup
 from datetime import date
 #from dateutil.parser import parse
-#import requests
+import requests
 from time import perf_counter
 #from urllib.parse import urljoin
 
-def scrape(start_date=date.today(), verbose=0): # todo
+def scrape(start_date=date.min, verbose=0): # todo
     '''
     Cross Infinite World has no calendar on their website.
     Physical and digital releases only available on Amazon.
@@ -21,4 +26,29 @@ def scrape(start_date=date.today(), verbose=0): # todo
 
     if verbose > 0:
         print(f'{len(result)} release(s) found for Cross Infinite World, completed in {perf_counter()-start_time} seconds.')
+    return result
+
+def series(url, start_date=date.min):
+    '''
+    Takes in a series url
+    (ex: http://www.crossinfworld.com/Reincarnated-as-the-Last-of-My-Kind.html).
+    To be completed later.
+    Returns empty list:
+    []
+    '''
+
+    result = []
+
+    return result
+
+def volume(url):
+    '''
+    Takes in a volume url
+    (ex: http://www.crossinfworld.com/Reincarnated-as-the-Last-of-My-Kind.html).
+    To be completed later.
+    Returns empty dictionary:
+    {}
+    '''
+
+    result = {}
     return result
--- scrape/dark_horse.py
+++ scrape/dark_horse.py
@@ -1,64 +1,83 @@
+"""
+To Do:
+- .get_format() does not work with digital releases; need to find a way to detect them
+- need a way to find new active licenses
+"""
+
 from bs4 import BeautifulSoup
 from datetime import date
 from dateutil.parser import parse
 import requests
 from time import perf_counter
 from urllib.parse import urljoin
+from utils import get_format
 
-def scrape(start_date=date.today(), verbose=0):
+def scrape(start_date=date.min, verbose=False):
     """
     Dark Horse has no calendar or way to search for only light novels (the also release many other types of books).
-    Runs a search for Vampire Hunter D (Dark Horse's only active license) and searches that page for upcoming releases.
+    Manually saves all known active licenses and runs them through the .series() method.
     Returns a list of dictionaries:
-    [{'date', 'title', 'volume', 'publisher':'Book Walker', 'store_link', 'format':'Digital'}]
+    [{'date':datetime.date, 'title':string, 'volume':string, 'publisher':'Dark Horse', 'publisher_link':string, 'format':string}]
     """
     
     start_time = perf_counter()
-    if verbose > 1:
-        print('Starting scraping for Dark Horse.')
+    if verbose: print('Starting scraping for Dark Horse.')
+
     result = []
-    url = f'https://www.darkhorse.com/Search/Browse/"vampire+hunter+d"---Books---{start_date.strftime("%B+%Y")}-December+2099/Ppydwkt8'
-    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+    series = ['Vampire Hunter D']
+
+    for title in series:
+        series_result = series(title, start_date)
+        result.apend(series_result)
     
-    items = soup.find_all('div', class_='list_item')
-    if verbose > 2:
-        print(f'Found {len(items)} items.')
-    for item in items:
-        href = item.find('a')['href']
-        result.append(dark_horse_volume(urljoin(url, href), start_date))
-    
-    if verbose > 0:
-        print(f'{len(result)} release(s) found for Dark Horse, completed in {perf_counter()-start_time} seconds.')
+    if verbose: print(f'{len(result)} release(s) found for Dark Horse, completed in {perf_counter()-start_time} seconds.')
     return result
 
-def volume_scrape(url, start_date=date.today()): # todo: need to double check on format, check if release date is before start date
+def series(title, start_date=date.min):
     """
-    Dark Horse calendar does not have all information on it,
-    need to check individual volume pages to get release date.
-    Returns a dictionaries:
-    {'date', 'title', 'volume', 'publisher':'Dark Horse', 'store_link', 'format':'Physical & Digital'}
+    Takes a Dark Horse series title
+    (ex: Vampire Hunter D),
+    searches for releases after or on the specified date,
+    and scrapes each volume individually for needed information.
+    Returns a list of dictionaries:
+    [{'date':datetime.date, 'title':string, 'volume':string, 'publisher':'Dark Horse', 'publisher_link':string, 'format':string}]
+    """
+
+    result = []
+    url = f'https://www.darkhorse.com/Search/Browse/"{title.lower().replace(' ', '+')}"---Books---{start_date.strftime("%B+%Y")}-December+2099/Ppydwkt8'
+    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+    
+    items = soup.find_all('div', {'class':'list_item'})
+    for item in items:
+        url = urljoin(url, item.find('a')['href'])
+        result.append(volume(url))
+    
+    return result
+
+def volume(url):
+    """
+    Takes in a valid Dark Horse volume url
+    (ex: https://www.darkhorse.com/Books/3004-474/Vampire-Hunter-D-Volume-29-Noble-Front-TPB),
+    and scrapes all relavent information from the page.
+    Returns a dictionary:
+    {'date':datetime.date, 'title':string, 'volume':string, 'publisher':'Dark Horse', 'publisher_link':string, 'format':'Physical & Digital'}
     """
     
     result = []
     soup = BeautifulSoup(requests.get(url).text, 'html.parser')
     
-    title_text = soup.find('h2', class_='title').get_text(strip=True).split(':')[0]
-    if ' Volume ' in title_text:
-        title_text_split = title_text.split(' Volume ')
-        title = title_text_split[0]
-        volume = title_text_split[1].split(':')[0]
-    else:
-        title = title_text
-        volume = '1'
+    title_text = soup.find('h2', {'class':'title'}).get_text(strip=True).split(':')[0].split(' Volume ')
+    title = title_text_split[0]
+    volume = title_text_split[1] if len(title_text)>1 else '1'
     
-    details = soup.find('div', class_='product-meta').find_all('dd')
+    details = soup.find('div', {'class':'product-meta'}).find_all('dd')
     release_date = parse(details[0].get_text(strip=True)).date()
-    # format_type = details[1].get_text(strip=True)
+    format_type = get_format(details[1].get_text()) # digital isn't listed under format, so brute forced to physical and digital for now
     
     release = {'date': release_date,
                'title': title,
                'volume': volume,
                'publisher': 'Dark Horse',
-               'store_link': url,
+               'publisher_link': url,
                'format': 'Physical & Digital'}
     return release
